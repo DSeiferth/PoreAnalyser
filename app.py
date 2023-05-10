7b6c856608280b94de71eeaa90f0d63501b5c926
David Seiferth <david.seiferth@dtc.ox.ac.uk>
2023-05-10 21:32:28 +0100

    upload file

--- app.py
+++ app.py
@@ -29,3 +29,10 @@ fig = hole_analysis.analysis(names, labels=labels, path=path_save+dirs[0],
                        )
 
 st.pyplot(fig)
+
+uploaded_file = st.file_uploader("Choose a file", label_visibility="visible",  accept_multiple_files=True )
+
+if uploaded_files:
+   for uploaded_file in uploaded_files:
+       st.write("Filename: ", uploaded_file.name)
+

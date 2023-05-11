e087550bf71122ac72fee57947caafd6da19ba80
David Seiferth <david.seiferth@dtc.ox.ac.uk>
2023-05-11 11:56:01 +0100

    df output

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ import streamlit as st
 import hole_analysis as hole_analysis
 #import os
 
+@st.cache
+def convert_df(df):
+    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+    return df.to_csv().encode('utf-8')
+
 st.title("Pore Analysis with HOLE")
 #st.latex(r''' a+a r^1+a r^2+a r^3 ''')
 
@@ -34,9 +39,10 @@ if uploaded_files:
             f.write(uploaded_file.getbuffer())
     #st.write('Uploaded', names)
     try:
-        fig , csv = hole_analysis.analysis(names, labels=labels, path='', end_radius=end_radius, save='Uploaded', title='',legend_outside=True)
+        fig , df = hole_analysis.analysis(names, labels=labels, path='', end_radius=end_radius, save='Uploaded', title='',legend_outside=True)
         st.pyplot(fig)
         st.write("pathway ", csv)
+        csv = convert_df(df)
         st.download_button(
             label="Download pathway profile as CSV",
             data=csv,

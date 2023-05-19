0d85c2f2b95dc9dfb686622abfa386bfd6d24be0
David Seiferth <david.seiferth@dtc.ox.ac.uk>
2023-05-19 11:46:50 +0100

    from app to visu lib

--- visualization.py
+++ visualization.py
@@ -1,8 +1,11 @@
 import MDAnalysis
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import py3Dmol
 
 def write_pdb_with_pore_surface(path='', name='', end_radius=15, num_circle = 24):
-    conf = path + name + '.sph'
+    conf = path + name[:-4] + '.sph'
     top = conf
     sph = MDAnalysis.Universe(top, conf, topology_format='pdb', format='pdb') # tpr_resid_from_one=True
     radii = sph.atoms.occupancies
@@ -36,3 +39,40 @@ def write_pdb_with_pore_surface(path='', name='', end_radius=15, num_circle = 24
     sol2.atoms.positions = coord_array
     sel = sol2.select_atoms('name *')
     sel.write(path + name + '_circle.pdb')
+
+def plt_ellipsoid_pathway(df_res, f_size=22, title='', end_radius=15):
+    z = df_res['z']
+    a = df_res['a']
+    b = df_res['b']
+    #theta = df_res['theta']
+    #print(min(z), max(z))
+    fig, ax = plt.subplots()
+    ax.set_ylim([0, end_radius + 5])
+    plt.plot(z, a, label='Larger radius of ellipsoid')
+    plt.plot(z, b, label='Smaller radius of ellipsoid\n=radius of spherical probe')
+    plt.ylabel(r"Pathway radii ($\AA$)", fontsize=f_size)
+    plt.xlabel(r"z-coordinate ($\AA$)", fontsize=f_size)
+    title_str = 'Pathfinding with ellipsoidal probe'
+    if title!='':
+        title_str = title_str + '\n' + title
+    plt.title(title_str, fontsize=f_size)
+
+    ax.tick_params(axis='both', which='major', labelsize=f_size)
+    ax.legend(prop={'size': 12}) # loc='upper center'
+    fig.tight_layout()
+    #fig.savefig('GlyR/asymmetric_channel_ellipse_pathway.png', bbox_inches='tight')  
+    #plt.show()
+    return fig
+
+def pathway_visu(path, name):
+    with open(path+name) as ifile:
+            system = "".join([x for x in ifile])
+    xyzview = py3Dmol.view(height=800, width=800,) 
+    xyzview.addModelsAsFrames(system)
+    xyzview.setStyle({'model': -1}, {"cartoon": {'color': 'spectrum'}})
+    with open(path+name + '_circle.pdb') as ifile:
+        sph2 = "".join([x for x in ifile])
+    xyzview.addModelsAsFrames(sph2)
+    xyzview.addSurface(py3Dmol.SES,{'opacity':0.9,'color':'lightblue'}, {'model': -1})
+    xyzview.zoomTo()
+    return xyzview

b3e4072072455dfcded00435261c83ba0a328641
David Seiferth <david.seiferth@dtc.ox.ac.uk>
2023-12-01 14:34:46 +0000

    unit tests for ellipse lib

---
+++ tests/test_ellipsoid_optimisation.py
@@ -0,0 +1,9 @@
+import unittest
+#import PoreFinding_pdb as pf 
+import numpy as np
+import sys
+sys.path.append('ProbeParticleEllipsoid/')
+from ellipsoid_optimisation import ellipsoid_pathway
+
+
+

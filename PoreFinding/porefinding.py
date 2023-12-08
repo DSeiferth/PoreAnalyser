5a218cda2df5c038a8f52128656736332d6c3a3d
David Seiferth <david.seiferth@dtc.ox.ac.uk>
2023-12-08 15:50:27 +0000

    document class

--- PoreFinding/porefinding.py
+++ PoreFinding/porefinding.py
@@ -20,6 +20,49 @@ sys.path.append(bla+'ProbeParticleEllipsoid/')
 from ellipsoid_optimisation import ellipsoid_pathway
 
 class PoreAnalysis():
+    """
+    Class for pore analysis of a set of pdb models.
+    Parameters:
+    - pdb_array (list): List of file paths to the input PDB models.
+    - opt_method (str, optional): Optimization method for ellipsoid fitting. Default is 'nelder-mead'.
+    - align_bool (bool, optional): Flag indicating whether to align the models. Default is True.
+    - end_radius (float, optional): Radius of the spherical probe particle. Default is 15.
+    - pathway_sel (str, optional): Atom selection string for identifying the pathway. Default is 'protein'.
+    - path_save (str, optional): Path to save analysis results. Default is ''.
+    - num_circle (int, optional): Number of circles for point cloud visualization of pore surface. Default is 24.
+    - clipping (int, optional): Clipping value for 3D visualization of pore surface. Default is 100.
+
+    Attributes:
+    - pdb_array (list): List of file paths to the input PDB models.
+    - align_bool (bool): Flag indicating whether to align the models.
+    - end_radius (float): Radius of the spherical probe particle.
+    - pathway_sel (str): Atom selection string for identifying the pathway.
+    - path_save (str): Path to save analysis results.
+    - labels (list): List of labels derived from the input PDB file paths.
+    - names_aligned (list): List of aligned PDB file paths.
+    - opt_method (str): Optimization method for ellipsoid fitting.
+    - num_circle (int): Number of circles for point cloud visualization of pore surface.
+    - clipping (int): Clipping value for 3D visualization of pore surface.
+    - hole_fig (matplotlib.figure.Figure): Figure object for the hole analysis.
+    - hole_df (pandas.DataFrame): DataFrame containing hole analysis results.
+    - ellipsoid_dfs (dict): Dictionary containing ellipsoid analysis results for each model.
+
+    Methods:
+    - hole_analysis: Perform hole analysis on the set of PDB models.
+    - ellipsoid_analysis: Perform ellipsoid analysis on a specific PDB model.
+    - plt_pathway_ellipsoid: Plot ellipsoid analysis results for a specific model.
+    - pathway_visualisation: Visualize the pathway for a specific model.
+    - pathway_rendering: Render the pathway for a specific model.
+
+    Example:
+    >>> pdb_models = ['model1.pdb', 'model2.pdb']
+    >>> pore_analysis = PoreAnalysis(pdb_array=pdb_models, opt_method='nelder-mead')
+    >>> pore_analysis.hole_analysis()
+    >>> pore_analysis.ellipsoid_analysis(index_model=0)
+    >>> pore_analysis.plt_pathway_ellipsoid(index_model=0)
+    >>> pore_analysis.pathway_visualisation(index_model=0)
+    >>> pore_analysis.pathway_rendering(index_model=0)
+    """
     def __init__(self, pdb_array, opt_method='nelder-mead',
                  align_bool=True, end_radius=15, pathway_sel='protein',
                  path_save = '', 
@@ -51,7 +94,25 @@ class PoreAnalysis():
 
     def hole_analysis(self, plot_lines=True, legend_outside=False, title='', f_size=15):
         """
-        spherical probe particle
+        Perform hole analysis on the set of PDB models.
+        HOLE uses a spherical probe particle.
+
+        Parameters:
+        - plot_lines (bool, optional): Flag indicating whether to plot lines. Default is True.
+        - legend_outside (bool, optional): Flag indicating whether to place the legend outside the plot. Default is False.
+        - title (str, optional): Title for the plot. Default is an empty string.
+        - f_size (int, optional): Font size for the plot. Default is 15.
+
+        Returns:
+        None
+
+        Notes:
+        This method performs hole analysis on the set of PDB models and generates visualizations.
+        The results are stored in the attributes hole_fig and hole_df.
+
+        Example:
+        >>> pore_analysis = PoreAnalysis(pdb_array=['model1.pdb', 'model2.pdb'])
+        >>> pore_analysis.hole_analysis()
         """
         fig, df = hole_analysis.analysis(self.pdb_array, labels=self.labels, 
                                           path='', end_radius=self.end_radius, 
@@ -68,6 +129,27 @@ class PoreAnalysis():
     
     def ellipsoid_analysis(self, index_model=0, 
                            plot_lines=True, legend_outside=False, title='', f_size=15):
+        """
+        Perform ellipsoid analysis on a specific PDB model.
+
+        Parameters:
+        - index_model (int, optional): Index of the model in the pdb_array. Default is 0.
+        - plot_lines (bool, optional): Flag indicating whether to plot lines. Default is True.
+        - legend_outside (bool, optional): Flag indicating whether to place the legend outside the plot. Default is False.
+        - title (str, optional): Title for the plot. Default is an empty string.
+        - f_size (int, optional): Font size for the plot. Default is 15.
+
+        Returns:
+        None
+
+        Notes:
+        This method performs ellipsoid analysis on a specific PDB model and generates visualizations.
+        The results are stored in the ellipsoid_dfs attribute.
+
+        Example:
+        >>> pore_analysis = PoreAnalysis(pdb_array=['model1.pdb', 'model2.pdb'])
+        >>> pore_analysis.ellipsoid_analysis(index_model=0)
+        """
         ellipsoid_pathway(p=self.path_save, 
                         pdb_name = self.names_aligned[index_model], 
                         sph_name = self.names_aligned[index_model][:-4], 
@@ -90,11 +172,41 @@ class PoreAnalysis():
                                      fname=self.names_aligned[0]+'_pathway_ellipse.txt', num_circle = self.num_circle)
 
     def plt_pathway_ellipsoid(self, index_model=0, title='', f_size=15):
+        """
+        Plot ellipsoid analysis results for a specific model.
+
+        Parameters:
+        - index_model (int, optional): Index of the model in the pdb_array. Default is 0.
+        - title (str, optional): Title for the plot. Default is an empty string.
+        - f_size (int, optional): Font size for the plot. Default is 15.
+
+        Returns:
+        matplotlib.figure.Figure: Figure object for the plot.
+
+        Example:
+        >>> pore_analysis = PoreAnalysis(pdb_array=['model1.pdb', 'model2.pdb'])
+        >>> pore_analysis.ellipsoid_analysis(index_model=0)
+        >>> fig = pore_analysis.plt_pathway_ellipsoid(index_model=0)
+        """
         df_res = self.ellipsoid_dfs[self.labels[index_model]]
         fig = plt_ellipsoid_pathway(df_res, f_size=f_size, title=title, end_radius=self.end_radius)
         return fig 
     
     def pathway_visualisation(self, index_model=0, f_end='_circle.pdb'):
+        """
+        Visualize the pathway for a specific model.
+
+        Parameters:
+        - index_model (int, optional): Index of the model in the pdb_array. Default is 0.
+        - f_end (str, optional): File ending for the visualization file. Default is '_circle.pdb'.
+
+        Returns:
+        xyzview: Pathway visualization object.
+
+        Example:
+        >>> pore_analysis = PoreAnalysis(pdb_array=['model1.pdb', 'model2.pdb'])
+        >>> xyzview = pore_analysis.pathway_visualisation(index_model=0)
+        """
         xyzview = pathway_visu(path='', name=self.names_aligned[index_model], 
                                f_end=f_end, pathway_sel=self.pathway_sel,
                                clipping=self.clipping,
@@ -103,5 +215,13 @@ class PoreAnalysis():
         return xyzview
     
     def pathway_rendering(self, index_model=0, f_end='_circle.pdb', outname='out'):
+        """
+        Render the pathway for a specific model.
+
+        Parameters:
+        - index_model (int, optional): Index of the model in the pdb_array. Default is 0.
+        - f_end (str, optional): File ending for the visualization file. Default is '_circle.pdb'.
+        - outname
+        """
         render_visu(path='', name=self.names_aligned[index_model], 
                     f_end=f_end, outname=outname, streamlit=False)
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -5,8 +5,13 @@ Welcome to PoreFinding's documentation!
 pore profiles. 
 Over the last two decades, advances in structural biology along with recent artificial intelligence–driven structure prediction algorithms, such as AlphaFold, have revealed a plethora of 3-D ion channel and nanopore structures in different conformational states. However, in nearly every case, these structures still require functional annotation. Different tools, such as HOLE and CHAP, allow the analysis of the physical dimensions of the pore running through an ion channel. Here, we present a package that allows users to calculate the pore profile of any input structure. Based on the well-established HOLE programme, we add a new feature to capture pore asymmetry by using an ellipsoidal probe particle.
 
+You can try out the package without installing anything using a
+`interactive web-service <https://huggingface.co/spaces/DSeiferth/PoreFinding>`_
+hosted on huggingface. 
 
-It uses the `HOLE <https://github.com/osmart/hole2/>`_
+We introduce an algorithm that employs ellipsoidal probe particles, enabling a more comprehensive characterization of pore asymmetries. A constriction is more asymmetric for a larger difference between the smaller and larger radius of the ellipsoidal probe particle. 
+
+The package uses the `HOLE <https://github.com/osmart/hole2/>`_
 programme wrapped by `MDAnalysis <https://github.com/MDAnalysis/mdanalysis>`_.
 
 Check out the :doc:`usage` section for further information, including

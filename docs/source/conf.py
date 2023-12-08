37aa8d2fc1e0ea109f3ecfe12025288d5c591af9
David Seiferth <david.seiferth@dtc.ox.ac.uk>
2023-12-08 12:50:37 +0000

    do not import lib

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,4 +38,15 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
 
-import PoreFinding
+#import PoreFinding
+#Traceback (most recent call last):
+#  File "/home/docs/checkouts/readthedocs.org/user_builds/porefinding/envs/latest/lib/python3.10/site-packages/sphinx/config.py", line 356, in eval_config_file
+#    exec(code, namespace)  # NoQA: S102
+#  File "/home/docs/checkouts/readthedocs.org/user_builds/porefinding/checkouts/latest/docs/source/conf.py", line 41, in <module>
+#    import PoreFinding
+#  File "/home/docs/checkouts/readthedocs.org/user_builds/porefinding/checkouts/latest/PoreFinding/__init__.py", line 8, in <module>
+#    from .porefinding import PoreAnalysis
+#  File "/home/docs/checkouts/readthedocs.org/user_builds/porefinding/checkouts/latest/PoreFinding/porefinding.py", line 6, in <module>
+#    import hole_analysis as hole_analysis
+#ModuleNotFoundError: No module named 'hole_analysis'
+
